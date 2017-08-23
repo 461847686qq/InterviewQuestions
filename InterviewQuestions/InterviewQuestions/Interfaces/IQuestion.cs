8d525fd5e0776442587032af79d216cdf701a758
GaoJiaMing <GaoJiaMing@DESKTOP-AJEA61O>
2017-08-23 15:29:13 +0800

    初始化项目

---
+++ InterviewQuestions/InterviewQuestions/Interfaces/IQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestions.Interfaces
+{
+    public interface IQuestion
+    {
+        /// <summary>
+        /// 作答
+        /// </summary>
+        void Answer();
+        /// <summary>
+        /// 显示题目
+        /// </summary>
+        void Show();
+    }
+}
